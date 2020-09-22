a7a587f42f70a74b6f30ef5bb3e431054c96a9a4
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-22 00:06:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,16 @@
 # Write your methods here
 
+<<<<<<< HEAD
 def loop_message_five_times(message)
   counter = 0 
   while counter <= 5 
   puts "#{message}"
+=======
+def loop_message_five_times(array)
+  counter = 0 
+  while counter <= 5 
+  puts "Hello World."
+>>>>>>> fd7a7a84a7e63c16db4349ef3fea59d3bc16720c
   counter += 1 
 end
   
@@ -23,6 +30,7 @@ def output_array(array)
 end
 
 def return_string_array(array)
+<<<<<<< HEAD
 count = 0 
 new_array = []
 while count < array.length do 
@@ -31,3 +39,10 @@ while count < array.length do
 end
 new_array
 end 
+=======
+array.to_s
+
+
+  
+end
+>>>>>>> fd7a7a84a7e63c16db4349ef3fea59d3bc16720c
